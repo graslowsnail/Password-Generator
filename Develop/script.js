d709cf032f61f5ac4e6a87c9a284d9918866d68a
graslowsnail <graslowsnail@gmail.,com>
2022-08-28 22:53:18 -0500

    Got password to print.

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    // var password = generatePassword();
+    var password = makeid();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
@@ -15,3 +16,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+// this prints random characters
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+// console.log(makeid());
